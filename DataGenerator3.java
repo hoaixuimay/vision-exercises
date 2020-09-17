28b2fe6180d161b230060fa4f672b336d0b72e45
Hoai Pham <hoaixuimay@gmail.com>
2020-09-17 21:36:01 +0700

    Add files via upload

---
+++ DataGenerator3.java
@@ -0,0 +1,56 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Same all connectivity
+ * 1 more connect at the end
+ */
+public class DataGenerator3 {
+
+    public static void main(String[] args) throws IOException{
+        int N = 10000;
+
+        FileOutputStream file = new FileOutputStream("tc5_input.txt");
+
+        int count = 0;
+
+        ArrayList<String> content = new ArrayList<String>();
+        // connected
+        for(int i = 1; i <= N; i=i+5){
+
+            content.add((i+1) + " " + i);
+            content.add((i+2) + " " + i);
+            content.add((i+3) + " " + i);
+            content.add((i+4) + " " + i);
+
+            content.add((i+2) + " " + (i+1));
+            content.add((i+3) + " " + (i+1));
+            content.add((i+4) + " " + (i+1));
+
+            content.add((i+3) + " " + (i+2));
+            content.add((i+4) + " " + (i+2));
+
+            content.add((i+4) + " " + (i+3));
+
+                count = count+10;
+            }
+
+        // final connected
+        content.add((N) + " " + (N-6));
+        count++;
+
+
+        file.write((N+"").getBytes());
+        file.write("\n".getBytes());
+        file.write((count+"").getBytes());
+        file.write("\n".getBytes());
+
+        for(String c: content){
+            file.write(c.getBytes());
+            file.write("\n".getBytes());
+        }
+        file.close();
+    }
+}
---
+++ DataGenerator4.java
@@ -0,0 +1,61 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Connect latest to 0
+ */
+public class DataGenerator4 {
+
+    public static void main(String[] args) throws IOException{
+        int N = 10000;
+
+        int half = N / 2;
+        FileOutputStream file = new FileOutputStream("tc6_input.txt");
+
+        int count = 0;
+
+        ArrayList<String> content = new ArrayList<String>();
+        // connected
+        for(int i = 1; i <= (half - 1); i++){
+            for(int j =(i+1); j <= half; j++){
+                String wr;
+                wr = j + " " + i;
+                    content.add(wr);
+                count++;
+            }
+        }
+
+        // partial connected
+        int step = 100;
+        for(int i = half + 1; i <= N; i = i + step){
+            // 5001 -> 5100; 5101 -> 5200; ..
+            int max = i + (step -1);
+            for(int j = i; j < max; j++){
+                for(int k = j+1; k <= max; k++ ){
+                    String wr;
+
+                    wr = k + " " + j;
+                    content.add(wr);
+                    count++;
+                }
+            }
+        }
+
+        content.add(N + " " + 1);
+        count++;
+
+
+        file.write((N+"").getBytes());
+        file.write("\n".getBytes());
+        file.write((count+"").getBytes());
+        file.write("\n".getBytes());
+
+        for(String c: content){
+            file.write(c.getBytes());
+            file.write("\n".getBytes());
+        }
+        file.close();
+    }
+}
